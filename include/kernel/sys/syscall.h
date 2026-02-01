04341a5718a459102622b5278ebf4f7bac9b2cea
matteoepitech <matteo.tutti@epitech.eu>
2026-02-01 16:46:55 +0100

    feat(syscall): add the syscall table

--- include/kernel/sys/syscall.h
+++ include/kernel/sys/syscall.h
@@ -14,14 +14,19 @@
 /* @brief Syscall function typedef */
 typedef int32_t (*syscall_fn_t)(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
 
-/* Syscall entry points (register-based, six arguments) */
-int32_t ksys_chdir(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
+/*
+ * @brief Array of syscalls. The position of this array determine the ID of the syscall in the EAX register
+ */
+extern const syscall_fn_t ksyscalls_array[];
+
+/* Syscall entry points (register-based, six arguments) in order. */
 int32_t ksys_open(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
 int32_t ksys_close(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
-int32_t ksys_fstat(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
-int32_t ksys_stat(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
+int32_t ksys_chdir(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
 int32_t ksys_mkdir(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
-int32_t ksys_unlink(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
 int32_t ksys_rmdir(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
+int32_t ksys_stat(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
+int32_t ksys_fstat(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
+int32_t ksys_unlink(sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t, sysarg_t);
 
 #endif /* ifndef KERNEL_SYS_SYSCALL_H_ */
--- src/kernel/sys/syscall/syscall.c
+++ src/kernel/sys/syscall/syscall.c
@@ -6,3 +6,17 @@
 */
 
 #include <kernel/sys/syscall.h>
+
+/*
+ * @brief Array of syscalls. The position of this array determine the ID of the syscall in the EAX register
+ */
+const syscall_fn_t ksyscalls_array[] = {
+    ksys_open,      // 1
+    ksys_close,     // 2
+    ksys_chdir,     // 3
+    ksys_mkdir,     // 4
+    ksys_rmdir,     // 5
+    ksys_stat,      // 6
+    ksys_fstat,     // 7
+    ksys_unlink,    // 8
+};
